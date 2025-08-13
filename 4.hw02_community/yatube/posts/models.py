b711a66fe050df914dc4c5541100e9489e4b33b0
dayterr <mvdayter@gmail.com>
2025-08-13 14:10:06 +0300

    collectiong course

---
+++ 4.hw02_community/yatube/posts/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(verbose_name='Название соообщества',
+                             max_length=200)
+    slug = models.SlugField(verbose_name='Адрес сообщества', unique=True)
+    description = models.TextField(verbose_name='Описание сообщества')
+
+    class Meta:
+        verbose_name = 'Post Group'
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField(verbose_name='Текст поста')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    author = models.ForeignKey(User, verbose_name='Автор поста',
+                               on_delete=models.CASCADE,
+                               related_name='posts',
+                               null=True)
+    group = models.ForeignKey(Group, verbose_name='Сообщество',
+                              on_delete=models.SET_NULL,
+                              blank=True, null=True,
+                              related_name='posts')
+
+    class Meta:
+        ordering = ('-pub_date',)
---
+++ 4.hw02_community/yatube/posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
+
+def index(request):
+    posts = Post.objects.all()[:10]
+    return render(request, 'index.html', {'posts': posts})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:12]
+    return render(request, 'group.html', {'group': group, 'posts': posts})
